03b33b98b4ab41053cc65341dcac9b4ab2221e3b
Ivan Kudriavtsev <kudrjavtsevvanek@gmail.com>
2022-10-06 23:03:45 +0300

    Hi!

--- app/src/main/java/com/example/example/MainActivity.kt
+++ app/src/main/java/com/example/example/MainActivity.kt
@@ -10,45 +10,45 @@ import com.example.example.animal.Gorilla
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var makakaArray: Array<Button>
-    private lateinit var dogArray: Array<Button>
-    private lateinit var elephant: Array<Button>
+    private lateinit var gorillaArr: Array<Button>
+    private lateinit var leopardArr: Array<Button>
+    private lateinit var rhinoArr: Array<Button>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        dogArray = arrayOf(findViewById<Button>(R.id.btn_0), findViewById<Button>(R.id.btn_1))
-        elephant = arrayOf(findViewById(R.id.btn_2), findViewById(R.id.btn_3))
-        makakaArray = arrayOf(findViewById<Button>(R.id.btn_4), findViewById<Button>(R.id.btn_5))
+        leopardArr = arrayOf(findViewById<Button>(R.id.btn_0), findViewById<Button>(R.id.btn_1))
+        rhinoArr = arrayOf(findViewById(R.id.btn_2), findViewById(R.id.btn_3))
+        gorillaArr = arrayOf(findViewById<Button>(R.id.btn_4), findViewById<Button>(R.id.btn_5))
 
         click()
     }
 
 
     private fun click() {
-        dogArray.first().setOnClickListener {
+        leopardArr.first().setOnClickListener {
             val sumatranRhino = SumatranRhino()
             sumatranRhino.animalActivity("Sumatran rhino")
         }
-        dogArray.last().setOnClickListener {
+        leopardArr.last().setOnClickListener {
             val sumatranRhino = SumatranRhino()
             sumatranRhino.eatSomething("Sumatran rhino")
         }
-        makakaArray.first().setOnClickListener {
+        gorillaArr.first().setOnClickListener {
             val gorilla = Gorilla()
             gorilla.animalActivity("Gorilla")
         }
-        makakaArray.last().setOnClickListener {
+        gorillaArr.last().setOnClickListener {
             val gorilla = Gorilla()
             gorilla.eatSomething("Gorilla")
         }
-        elephant.first().setOnClickListener {
+        rhinoArr.first().setOnClickListener {
             val amurLeopard = AmurLeopard()
             amurLeopard.animalActivity("Amur leopard")
         }
-        elephant.last().setOnClickListener {
+        rhinoArr.last().setOnClickListener {
             val amurLeopard = AmurLeopard()
             amurLeopard.eatSomething("Amur leopard")
         }
